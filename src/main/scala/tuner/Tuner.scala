54ec2d98a9457fb1470e7b0c6ad9fe1253d12f4d
Thomas Torsney-Weir <torsneyt@gmail.com>
2013-02-22 09:09:57 +0100

    remove java and package checks.  they don't really work

--- src/main/scala/tuner/Tuner.scala
+++ src/main/scala/tuner/Tuner.scala
@@ -53,6 +53,7 @@ object Tuner extends SimpleSwingApplication {
 
   def top = { 
     // Make sure R and rJava are installed otherwise all bets are off
+    /*
     if(!Rapp.pathOk) {
       RNotInstalledDialog
     } else if(!Rapp.jriOk) {
@@ -62,14 +63,17 @@ object Tuner extends SimpleSwingApplication {
     } else {
       val missingPackages = R.missingPackages
       if(missingPackages.isEmpty) {
-        //openProject(Project.fromFile("/Users/tom/Projects/tuner.jogl/test_data/britta_proj"))
+      */
+        //openProject(Project.fromFile("/Users/tom/Downloads/tom/"))
         ProjectChooser
+      /*
       } else {
         new InstallPackageDialog(missingPackages) {
           val installPackage = R.installPackage(_)
         }
       }
     }
+    */
   }
 
   def startNewSimProject = {
