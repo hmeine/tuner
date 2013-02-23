b94e5c9f547afa6b4c2553d5486bc3ed4d0efb10
Thomas Torsney-Weir <torsneyt@gmail.com>
2013-02-23 17:54:44 +0100

    local sensitivity window is colored by gradient lines

--- src/main/scala/tuner/gui/LocalSensitivityWindow.scala
+++ src/main/scala/tuner/gui/LocalSensitivityWindow.scala
@@ -4,13 +4,15 @@ import scala.swing.Frame
 import scala.swing.ScrollPane
 import scala.swing.Table
 
+import tuner.Color
+import tuner.Config
 import tuner.project.FunctionProject
 
 class LocalSensitivityWindow(project:FunctionProject) extends Frame {
 
   val measurementTable = {
     val initialData:Array[Array[Any]] = computeMeasures.map {case (fld,v) => 
-      (fld::decodeVals(v)).toArray.asInstanceOf[Array[Any]]
+      (renderLabel(fld)::decodeVals(v)).toArray.asInstanceOf[Array[Any]]
     }.toArray
     new Table(initialData, "Measure"::project.inputFields)
   }
@@ -26,11 +28,16 @@ class LocalSensitivityWindow(project:FunctionProject) extends Frame {
   def updateView = {
     computeMeasures.zipWithIndex foreach {case ((m,vs),i) =>
       val dvs = decodeVals(vs)
-      measurementTable.update(i, 0, m)
+      measurementTable.update(i, 0, renderLabel(m))
       dvs.zipWithIndex foreach {case (v,j) =>
         measurementTable.update(i, j+1, v)
       }
     }
+  }
+
+  private def renderLabel(label:String) = {
+    val color = Config.sensitivityColors.getOrElse(label, Color.Black)
+    "<html><span style='color:" + color.toCss + "'>" + label + "</span></html>"
   }
 
   private def decodeVals(vals:Map[String,Float]) : List[String] = {
